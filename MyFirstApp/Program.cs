3eae05d8c0835960144330e8737f3a6e3e567928
LPanduwu <liisbeth.prangli@tktk.ee>
2023-01-29 01:41:30 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//This is a comment to my code
+//Rakendus palub kasutajal sisestada tema nime
+//Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+//string interpolation
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+Console.WriteLine($"Hello, {userName}!"); //Output
